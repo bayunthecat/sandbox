2e573b16a3078e239fd2e569bfc667cb79a2903e
Vadym Romanchuk <vromanchuk@quotemedia.com>
2023-12-04 20:47:55 -0800

    fix peak finder, implement 2d peak finder

--- src/main/java/com/mine/lectures/PeakFinder.java
+++ src/main/java/com/mine/lectures/PeakFinder.java
@@ -1,38 +1,51 @@
 package com.mine.lectures;
 
-import java.util.Arrays;
-
 public class PeakFinder {
 
-    public int findAPeakRecursive(int[] array) {
-        int half = array.length / 2;
-        int value = array[half];
-        if (half - 1 >= 0 && value < array[half - 1]) {
-            return findAPeakRecursive(Arrays.copyOfRange(array, 0, half));
-        } else if (half + 1 < array.length && value < array[half + 1]) {
-            return findAPeakRecursive(Arrays.copyOfRange(array, half, array.length));
-        } else {
-            return half;
-        }
-    }
+	public int findAPeakRecursive(int[] array) {
+		return findAPeakRecursive(array, 0, array.length);
+	}
 
-    public int fundAPeakCycle(int[] array) {
-        int result = -1;
-        int[] divided = array;
-        do {
-            int half = divided.length / 2;
-            if (half - 1 > 0 && divided[half] < divided[half - 1]) {
-                divided = Arrays.copyOfRange(divided, 0, half);
-            } else if (half + 1 < divided.length && divided[half] < divided[half + 1]) {
-                divided = Arrays.copyOfRange(divided, half, divided.length);
-            } else {
-                return half;
-            }
-        } while (divided.length != 1);
-        return result;
-    }
+	private int findAPeakRecursive(int[] array, int from, int to) {
+		int half = from + ((to - from) / 2);
+		int value = array[half];
+		if (half - 1 >= 0 && value < array[half - 1]) {
+			return findAPeakRecursive(array, from, half);
+		} else if (half + 1 < array.length && value < array[half + 1]) {
+			return findAPeakRecursive(array, half, to);
+		} else {
+			return half;
+		}
+	}
 
-    public int[] findAPeak(int[][] array) {
-        return new int[] {0, 0};
-    }
+	public int findAPeakCycle(int[] array) {
+		int to = array.length, from = 0;
+		do {
+			int half = from + ((to - from) / 2);
+			if (half - 1 >= 0 && array[half] < array[half - 1]) {
+				to = half;
+			} else if (half + 1 < array.length && array[half] < array[half + 1]) {
+				from = half;
+			} else {
+				return half;
+			}
+		} while (true);
+	}
+
+	public int[] findAPeak(int[][] array) {
+		int from = 0, to = array.length, half, maxIndex;
+		do {
+			half = from + ((to - from) / 2);
+			int[] column = array[half];
+			maxIndex = findAPeakCycle(column);
+			if (maxIndex - 1 >= 0 && array[half][maxIndex - 1] > array[half][maxIndex]) {
+				to = half;
+			} else if (maxIndex + 1 < to && array[half][maxIndex + 1] > array[half][maxIndex]) {
+				from = half;
+			} else {
+				break;
+			}
+		} while (true);
+		return new int[]{half, maxIndex};
+	}
 }
--- src/test/java/com/mine/lectures/PeakFinderTest.java
+++ src/test/java/com/mine/lectures/PeakFinderTest.java
@@ -6,50 +6,80 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class PeakFinderTest {
 
-    PeakFinder subject = new PeakFinder();
+	PeakFinder subject = new PeakFinder();
 
-    @Test
-    void test_1() {
-        assertThat(subject.findAPeakRecursive(new int[] {-1, 2, 3, 7, 11, 3, 2, -1, -11})).isEqualTo(4);
-    }
+	@Test
+	void test_1_1() {
+		assertThat(subject.findAPeakRecursive(new int[]{-1, 2, 3, 7, 11, 3, 2, -1, -11})).isEqualTo(4);
+	}
 
-    @Test
-    void test_2() {
-        assertThat(subject.findAPeakRecursive(new int[] {-1, 11, 10, 9, 8, 7, 6, 5, -11})).isEqualTo(1);
-    }
+	@Test
+	void test_1_2() {
+		assertThat(subject.findAPeakRecursive(new int[]{-1, 11, 10, 9, 8, 7, 6, 5, -11})).isEqualTo(1);
+	}
 
-    @Test
-    void test_1_1() {
-        assertThat(subject.fundAPeakCycle(new int[] {-1, 2, 3, 7, 11, 3, 2, -1, -11})).isEqualTo(4);
-    }
+	@Test
+	void test_1_3() {
+		assertThat(subject.findAPeakRecursive(new int[]{4, 3, 2, 1, 0, -1, -2, -3, -4})).isEqualTo(0);
+	}
 
-    @Test
-    void test_2_1() {
-        assertThat(subject.fundAPeakCycle(new int[] {-1, 11, 10, 9, 8, 7, 6, 5, -11})).isEqualTo(1);
-    }
+	@Test
+	void test_1_4() {
+		assertThat(subject.findAPeakRecursive(new int[]{-4, -3, -2, -1, 0, 1, 2, 3, 4})).isEqualTo(8);
+	}
 
-    @Test
-    void test2d_1() {
-        int[][] array = new int[][] {
-                {0, 0, 0, 0, 0},
-                {0, 1, 1, 1, 0},
-                {0, 1, 2, 1, 0},
-                {0, 1, 1, 1, 0},
-                {0, 0, 0, 0, 0}
-        };
-        assertThat(subject.findAPeak(array));
-    }
+	@Test
+	void test_1_5() {
+		assertThat(subject.findAPeakRecursive(new int[]{-4, -3, -2, -1, 0, 1, 6, 3, 4})).isEqualTo(6);
+	}
 
-    @Test
-    void test2d_2() {
-        int[][] array = new int[][] {
-                {0, 0, 0, 0, 0},
-                {1, 1, 1, 0, 0},
-                {2, 1, 1, 0, 0},
-                {1, 1, 1, 0, 0},
-                {0, 0, 0, 0, 0}
-        };
-        assertThat(subject.findAPeak(array));
-    }
+	@Test
+	void test_2_1() {
+		assertThat(subject.findAPeakCycle(new int[]{-1, 2, 3, 7, 11, 3, 2, -1, -11})).isEqualTo(4);
+	}
+
+	@Test
+	void test_2_2() {
+		assertThat(subject.findAPeakCycle(new int[]{-1, 11, 10, 9, 8, 7, 6, 5, -11})).isEqualTo(1);
+	}
+
+	@Test
+	void test_2_3() {
+		assertThat(subject.findAPeakCycle(new int[]{4, 3, 2, 1, 0, -1, -2, -3, -4})).isEqualTo(0);
+	}
+
+	@Test
+	void test_2_4() {
+		assertThat(subject.findAPeakCycle(new int[]{-4, -3, -2, -1, 0, 1, 2, 3, 4})).isEqualTo(8);
+	}
+
+	@Test
+	void test_2_5() {
+		assertThat(subject.findAPeakCycle(new int[]{-4, -3, -2, -1, 0, 1, 6, 3, 4})).isEqualTo(6);
+	}
+
+	@Test
+	void test2d_1() {
+		int[][] array = new int[][]{
+				{0, 0, 0, 0, 0},
+				{0, 1, 1, 1, 0},
+				{0, 1, 2, 1, 0},
+				{0, 1, 1, 1, 0},
+				{0, 0, 0, 0, 0}
+		};
+		assertThat(subject.findAPeak(array)).containsExactly(2, 2);
+	}
+
+	@Test
+	void test2d_2() {
+		int[][] array = new int[][]{
+				{0, 0, 0, 0, 0},
+				{1, 3, 2, 1, 0},
+				{4, 3, 2, 1, 0},
+				{1, 3, 2, 1, 0},
+				{0, 0, 0, 0, 0}
+		};
+		assertThat(subject.findAPeak(array)).containsExactly(2, 0);
+	}
 
 }
