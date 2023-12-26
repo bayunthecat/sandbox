3fa2273ca7c33d7b408793b4f5ef74882819aa48
Vadym Romanchuk <vadym.romanchuk95@gmail.com>
2023-12-26 12:55:16 -0800

    maximum score after split problem with memoization

--- src/main/java/com/mine/leet/daily/NumberOfDiceRollsWithTargetSum.java
+++ src/main/java/com/mine/leet/daily/NumberOfDiceRollsWithTargetSum.java
@@ -2,30 +2,39 @@ package com.mine.leet.daily;
 
 import jakarta.enterprise.context.ApplicationScoped;
 
+import java.util.Arrays;
+
 @ApplicationScoped
 public class NumberOfDiceRollsWithTargetSum {
 
-    public int numRollsToTarget(int n, int k, int target) {
-        return numRollsToTargetRecursion(n, k, target);
-    }
+	private static final int MOD = (int) (Math.pow(10, 9) + 7);
 
-    private int numRollsToTargetRecursion(int n, int k, int target) {
-        if (n == 0) {
-            return 0;
-        }
-        if (n == 1 && target <= k) {
-            return 1;
-        }
+	public int numRollsToTarget(int n, int k, int target) {
+		int[][] memo = new int[n + 1][target + 1];
+		for (int[] ints : memo) {
+			Arrays.fill(ints, -1);
+		}
+		return numRollsToTargetRecursion(n, k, target, memo);
+	}
 
-        int rolls = 0;
-        for (int i = 1; i <= k; i++) {
-            int remains = target - i;
-            if (remains > 0) {
-                rolls += numRollsToTarget(n - 1, k, remains);
-            } else {
-                break;
-            }
-        }
-        return rolls;
-    }
+	private int numRollsToTargetRecursion(int n, int k, int target, int[][] memo) {
+		if (n == 1 && target <= k) {
+			return 1;
+		}
+		if (n < 0) {
+			return 0;
+		}
+		if (memo[n][target] != -1) {
+			return memo[n][target];
+		}
+		int rolls = 0;
+		for (int i = 1; i <= k; i++) {
+			int remains = target - i;
+			if (remains > 0) {
+				rolls = (rolls + numRollsToTargetRecursion(n - 1, k, remains, memo)) % MOD;
+			}
+		}
+		memo[n][target] = rolls;
+		return rolls;
+	}
 }
--- src/test/java/com/mine/leet/daily/NumberOfDiceRollsWithTargetSumTest.java
+++ src/test/java/com/mine/leet/daily/NumberOfDiceRollsWithTargetSumTest.java
@@ -9,30 +9,48 @@ import static org.assertj.core.api.Assertions.assertThat;
 @QuarkusTest
 class NumberOfDiceRollsWithTargetSumTest {
 
-    @Inject
-    NumberOfDiceRollsWithTargetSum subject;
+	@Inject
+	NumberOfDiceRollsWithTargetSum subject;
 
-    @Test
-    void test_1() {
-        final var result = subject.numRollsToTarget(2, 6, 7);
-        assertThat(result).isEqualTo(6);
-    }
+	@Test
+	void test_1() {
+		final var result = subject.numRollsToTarget(2, 6, 7);
+		assertThat(result).isEqualTo(6);
+	}
 
-    @Test
-    void test_2() {
-        final var result = subject.numRollsToTarget(3, 6, 8);
-        assertThat(result).isEqualTo(21);
-    }
+	@Test
+	void test_2() {
+		final var result = subject.numRollsToTarget(3, 6, 8);
+		assertThat(result).isEqualTo(21);
+	}
 
-    @Test
-    void test_3() {
-        final var result = subject.numRollsToTarget(3, 6, 150);
-        assertThat(result).isEqualTo(0);
-    }
+	@Test
+	void test_2_memo() {
+		final var result1 = subject.numRollsToTarget(3, 6, 8);
+		assertThat(result1).isEqualTo(21);
+	}
 
-    @Test
-    void test_6() {
-        final var result = subject.numRollsToTarget(2, 6, 5);
-        assertThat(result).isEqualTo(4);
-    }
+	@Test
+	void test_3() {
+		final var result = subject.numRollsToTarget(3, 6, 150);
+		assertThat(result).isEqualTo(0);
+	}
+
+	@Test
+	void test_4() {
+		final var result = subject.numRollsToTarget(2, 6, 5);
+		assertThat(result).isEqualTo(4);
+	}
+
+	@Test
+	void test_5() {
+		final var result = subject.numRollsToTarget(4, 6, 8);
+		assertThat(result).isEqualTo(35);
+	}
+
+	@Test
+	void test_6() {
+		final var result = subject.numRollsToTarget(30, 30, 500);
+		assertThat(result).isEqualTo(222616187);
+	}
 }
